96f65c0fab0927e7e64753ba5e1e6732893fd2d6
Emily Lo <hiemilylo@gmail.com>
2019-10-03 15:07:16 -0700

    tabs in floor detail page

--- floor.js
+++ floor.js
@@ -9,3 +9,20 @@ for (const image of imgs) {
         prod_image.src = image.src
     }
 }
+
+function openTab(evt, cityName) {
+    let i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
